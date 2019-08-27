e820b19d3ce48b56ef5575886ce9a645e20adae0
Paula-18 <paunahappy@gmail.com>
2019-08-27 12:43:45 -0700

    piano working

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let btn = document.getElementById('btnDo');
+const buttons = document.querySelectorAll('button');
 
-btn.addEventListener('click',() => {alert ('yolo')});
+const playnote = ()=>{
+
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+    //console.log(audioId)
+    console.log(audioId);
+    const audio = document.getElementById(audioId);
+    console.log(audio);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+}
+
+buttons.forEach(
+    button => button.addEventListener('click', playnote) 
+);
